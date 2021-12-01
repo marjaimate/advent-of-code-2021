7858aa58e3212bc7cccad354b597dcca26c4a330
Máté Marjai <mate@theproduct.works>
2021-12-01 16:06:10 +0100

    Rework for the second part, using the count_increases function that is derived from the initial get_increases. Using map and each_con again to get the sum of the windows.

--- day1/measurements.rb
+++ day1/measurements.rb
@@ -1,14 +1,26 @@
 class Measure
   def initialize(file_name)
-    @measurements = File.readlines(file_name)
+    @measurements = File.readlines(file_name).map(&:to_i)
   end
 
   def get_increases
-    @measurements.each_cons(2).map do |a, b|
-      b.to_i > a.to_i ? 1 : 0
+    count_increases(@measurements)
+  end
+
+  def get_window_increases
+    input = @measurements.each_cons(3).map do |a, b, c|
+      a + b + c
+    end
+    count_increases(input)
+  end
+
+  def count_increases(input)
+    input.each_cons(2).map do |a, b|
+      b > a ? 1 : 0
     end.inject(:+)
   end
 end
 
 task = Measure.new("input1.txt")
-puts task.get_increases
+puts "Increase count with window of 2: #{task.get_increases}"
+puts "Increase count with window of 3: #{task.get_window_increases}"
